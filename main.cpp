9a63ff18535e3206e18d8c6e951cb2475bd7fb90
TominKapp <81731981+TominKapp@users.noreply.github.com>
2021-06-01 01:55:37 -0700

    Update main.cpp
    Oops didnt do it b4

--- main.cpp
+++ main.cpp
@@ -1,37 +1,13 @@
 #include "GUI/header/gui_board.hpp"
 
-//ChessGame* game = nullptr;
 GUI_Board* game;
 
 int main(int argc, const char* args[]) {
-    /*
-    //game = new ChessGame();
-    //game->init(SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,800);
 
-    while(game->running()) {
-        game->handleEvents();
-        game->update();
-        game->render();
-    }
-
-    game->clean();
-    */
     game = new GUI_Board();
-    game->init(SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,800);
+    game->runGame();
 
-    
-    while (!game->gameOver(1) && game->running()) {
-        
-        game->handleEvents();
-        game->update();
-        game->render();
-        
-        game->updateBoardState();
-    }
-    game->drawDebugBoard();
-    
-
-    game->clean();
+    delete game;
 
     return 0;
 }
