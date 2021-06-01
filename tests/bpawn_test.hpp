b5394b20f308c5882b23dfda7eb21b64674e199d
meslane <merrickslane@gmail.com>
2021-06-01 03:26:27 -0700

    all tests passing

--- tests/bpawn_test.hpp
+++ tests/bpawn_test.hpp
@@ -4,12 +4,12 @@
 #include "../piece.hpp"
 #include <math.h>
 TEST(BlackPawnTest, testPosition) {
-    Piece p(0, 1, 0x1000000000000);
+    Piece p(0, 0, 0x1000000000000);
     EXPECT_EQ(0x1000000000000, p.getPosition());
 }
 
 TEST(BlackPawnTest, possibleMoves) {
-    Piece p(0, 1, 0x1000000000000);
-    EXPECT_EQ(static_cast<uint64_t>(pow(2, 8) | static_cast<uint64_t>(pow(2, 16))), p.getAllValidMoves(0x1000000000000, 0x1000000000000));
+    Piece p(0, 0, static_cast<uint64_t>(pow(2,63)));
+    EXPECT_EQ(static_cast<uint64_t>(pow(2, 55)) | static_cast<uint64_t>(pow(2, 47)), p.getAllValidMoves(static_cast<uint64_t>(pow(2,63)), static_cast<uint64_t>(pow(2,63))));
 }
 #endif
